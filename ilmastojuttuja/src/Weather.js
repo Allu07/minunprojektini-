db45b803eb0ee91df58bbc53f883ef7fe0901342
Allu07 <allukorpelainen928@gmail.com>
2020-10-20 18:13:29 +0300

    tehtiin cssää ja flexbox froggya

--- ilmastojuttuja/src/Weather.js
+++ ilmastojuttuja/src/Weather.js
@@ -8,7 +8,24 @@ class Weather extends Component {
           <div className="Weather_day">ti</div>
           <div className="Weather_time">9.00</div>
           <div className="Weather_temp">9°</div>
-          <img src='http://openweathermap.org/img/wn/09d.png' alt="logo" />
+          <img src='http://openweathermap.org/img/wn/09d.png' alt="logo"/>
+        </div>
+
+        <div className="Weather_forecast">
+
+
+          <div className="Weather_box">
+            <div className="Weather_day">su</div>
+            <div className="Weather_time">13:30</div>
+            <div className="Weather_temperature">5°</div>
+            <img src='http://openweathermap.org/img/wn/09d.png' alt="logo" />
+          </div>
+          <div className="Weather_box">
+            <div className="Weather_day">su</div>
+            <div className="Weather_time">13:30</div>
+            <div className="Weather_temperature">5°</div>
+            <img src='http://openweathermap.org/img/wn/09d.png' alt="logo" />
+          </div>
         </div>
       </div>
     )
